9241078cf3877ec90c8e87ee1b9398a3f8a8dccf
Adam Wulf <adam.wulf@gmail.com>
2020-01-07 00:24:43 -0600

    Initial Commit

---
+++ MMPageManager/MMPageManager.h
@@ -0,0 +1,19 @@
+//
+//  MMPageManager.h
+//  MMPageManager
+//
+//  Created by Adam Wulf on 1/7/20.
+//  Copyright © 2020 Milestone Made. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MMPageManager.
+FOUNDATION_EXPORT double MMPageManagerVersionNumber;
+
+//! Project version string for MMPageManager.
+FOUNDATION_EXPORT const unsigned char MMPageManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MMPageManager/PublicHeader.h>
+
+
